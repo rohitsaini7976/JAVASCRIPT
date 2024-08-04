c805ea72a1b9d17069731b10cfa584633c9621af
Rohit <sainirohit7976@gmail.com>
2024-08-04 16:40:07 +0530

    update

---
+++ 01class/first.js
@@ -0,0 +1,53 @@
+//  let name = "rohit saini"
+//  console.log(name)
+//  let age =20
+//  console.log(age)
+
+// const age =20;
+// console.log(age);
+// const PI=3.14;
+// console.log(PI);
+
+
+// {
+//     let a= 5;
+//    // let a=4;
+//     console.log(a);   
+// }
+// {
+//     let a=10;
+//     console.log(a);
+// }
+
+
+// const student={
+//     fullname: "Rohit saini",
+//     age:20,
+//     cgpa:9,
+//     isPass:true
+// };
+// student["age"]=student["age"]+1;
+// console.log(student["age"])
+
+
+//create a const object called "prduct" to store infromation shown in the picture.
+
+const product ={
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price :270,
+
+};
+console.log(product);
+
+//create a const object called "profie" to store information shown in the picture.
+
+const profile ={
+    username: "@rohit_saini_2003",
+    isFollow: false,
+    followers: 500,
+    following: 123
+
+}
+console.log( typeof profile["followers"])
