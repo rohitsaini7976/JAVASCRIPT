21448f57c2a1352ab984b16cb877a8b06fcc90ea
Rohit <sainirohit7976@gmail.com>
2024-08-06 14:42:55 +0530

    update

---
+++ 03class/script.js
@@ -0,0 +1,92 @@
+//Loops in js
+
+//for(let count =1; count<=100; count++){
+//     console.log("ROHIT SAINI");
+    
+// }
+// console.log("loop has ended");
+
+// let sum =0;
+// let n =100;
+// for(let i=1;i<=n;i++){
+//      sum =sum+i;
+// }
+// console.log("sum =",sum);
+// console.log("LOOP HAS ENDED");
+
+// let i =1;
+// while(i<=5){
+//     console.log("i=",i);
+//     i++;
+// }
+
+
+
+// i =1;
+// do{
+//   console.log("i=",i);
+//   i++;
+// }while(i<=10);
+
+//for-of loop
+// let str ="JavaScript";
+// let size =0;
+// for(let val of str){
+//     console.log("val=",val);
+//     size++;
+// }
+// console.log("string size=",size);
+
+//for in loop
+// let student ={
+//     name:"Rohit",
+//     age:20,
+//     cgpa:7.5,
+//     isPass:true
+// };
+// for(let key in student){
+//     console.log("key=",key,"value:",student[key]);
+// }
+
+// question:1 print all even number from 0 to 100.
+
+// num=100;
+// for(let num=0;num<=100;num++)
+// {
+//     if(num%2===0){
+//         console.log("num is even=",num);
+//     }
+// }
+
+//question:2 Create a game where you start with any random game number. Ask the user to keep guessing the game number until the user enters correct value.
+
+// let gameNum =25;
+
+// let userNum = prompt("Guess the game number: ");
+// console.log(userNum);
+
+// while(userNum !=gameNum){  //game
+// userNum= prompt(" You entered wrong number. Guess again :");
+
+// }
+// console.log("Congratulations, you enter the right number");
+
+//Strings 
+
+// let str ="Rohit saini";
+// console.log(str[10]);
+
+// Template Literals
+// let specialString = `This is a template literal `;
+// console.log(specialString)
+
+// let obj ={
+//     item : "Pen",
+//     price:10,
+// };
+// let output =`the cost of ${obj.item} is ${obj.price} rupes`;
+// console.log(output);
+//console.log("The cost of",obj.item,"is",obj.price,"rupes");
+
+
+
